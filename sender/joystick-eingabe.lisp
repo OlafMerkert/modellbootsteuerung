cd30bc0c724080e9b77c264302253314f89f5281
Olaf Merkert <olaf@m-merkert.de>
2012-07-18 16:00:24 +0200

    using a window is required, so the js input can be picked up

--- sender/joystick-eingabe.lisp
+++ sender/joystick-eingabe.lisp
@@ -7,14 +7,37 @@
 (in-package :joystick-eingabe)
 
 
-(defun experiment-fn ()
-  "figure out how to use sdl"
-  (sdl:with-init (sdl:sdl-init-joystick)
-    (sdl:with-events ()
-      (:quit-event () t)
-      (:joy-axis-motion-event
-       (:which joystick :axis axis :value value)
-       (format t "Joystick ~A moved axis ~A to ~D" joystick axis value)))))
+(defun joystick-main-loop (js-spec data)
+  "listen for joystick events, put the axis data into DATA according
+to JS-SPEC and call the SEND method on DATA from time to time."
+  (destructuring-bind (&key id gas ruder) js-spec
+   (sdl:with-init (sdl:sdl-init-joystick)
+     (sdl:window 400 100
+                 :title-caption "Hello Katja"
+                 :icon-caption  "Hello Katja")
+     (setf (sdl:frame-rate) 30)
+     (sdl-cffi::sdl-joystick-event-state sdl-cffi::sdl-enable)
+     (let ((js   (sdl-cffi::sdl-joystick-open id)))
+       (sdl:with-events ()
+         (:quit-event ()
+                      (when js
+                        (sdl-cffi::sdl-joystick-close js))
+                      t)
+         (:joy-axis-motion-event
+          (:which joystick :axis axis :value value)
+          (cond ((eql axis gas)
+                 (setf (gas data) value))
+                ((eql axis ruder)
+                 (setf (ruder data) value))))
+         (:idle ()
+                (sdl:clear-display sdl:*black*)
+                (sdl:draw-string-solid-* (format nil "Hello Katja")
+                                         200 50
+                                         :color sdl:*white*
+                                         :justify :center)
+                (sdl:update-display))
+         (:video-expose-event ()
+                              (sdl:update-display)))))))
 
 (defun list-joysticks ()
   "list all the joysticks found on this system, with name."
